9917463d882453bbe0b7a61d177c0227e78d58b2
lk23286 <lk23286@gmail.com>
2023-06-19 12:28:21 +0200

    add table

--- StoreTableItemsInPlist/Controller/TableViewController.swift
+++ StoreTableItemsInPlist/Controller/TableViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    var itemArray = ["Apple", "Banan"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //MARK: - Table
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell")
+        
+        cell?.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell!
+    }
+    
+    
 
 }
 
