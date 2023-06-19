58d477a358ca500517e26b60fc6012b52c9b8644
lk23286 <lk23286@gmail.com>
2023-06-19 12:08:16 +0200

    Initial Commit

---
+++ StoreTableItemsInPlist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreTableItemsInPlist
+//
+//  Created by Laszlo Kovacs on 2023. 06. 19..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
