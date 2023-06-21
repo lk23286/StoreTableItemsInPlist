7109087e51f94a25c68a52b34a474573c4313bd6
lk23286 <lk23286@gmail.com>
2023-06-21 11:31:57 +0200

    create loadItem function to load item.plist data. Items are stored in item.plist file Finish this part.

--- StoreTableItemsInPlist/Controller/TableViewController.swift
+++ StoreTableItemsInPlist/Controller/TableViewController.swift
@@ -8,40 +8,24 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-    
-   var itemArray = [Item]()
-    
-    //["Apple", "Banan"]
-    
-    //var defaults = UserDefaults.standard
+    var itemArray = [Item]()
     
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last?.appendingPathComponent("item.plist")
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(dataFilePath!)
-        
-        let item = Item()
-        item.title = "Apple"
-        itemArray.append(item)
-        
-//        if let items = defaults.array(forKey: "TodoItems") as? [String] {
-//            itemArray = items
-//        }
-      
+        loadItems()
     }
-
+    
     //MARK: - Table
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         itemArray.count
-        
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell")
-        
         let item = itemArray[indexPath.row]
         
         cell?.textLabel?.text = item.title
@@ -49,26 +33,18 @@ class TableViewController: UITableViewController {
         cell?.accessoryType = item.done ? .checkmark: .none
         
         return cell!
-        
     }
     
     //MARK: - Checkmark
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-      
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         tableView.deselectRow(at: indexPath, animated: true)
         
-//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-//        } else {
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-//        }
-        
         saveItems()
-        
     }
+    
     //MARK: - Add Item
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -79,14 +55,12 @@ class TableViewController: UITableViewController {
         let action = UIAlertAction(title: "Add", style: .default) { alert in
             
             if let newTextField = textField.text {
-                
                 let newItem = Item()
                 newItem.title = newTextField
                 self.itemArray.append(newItem)
             }
-           
-            self.saveItems()
             
+            self.saveItems()
         }
         
         alert.addAction(action)
@@ -99,21 +73,31 @@ class TableViewController: UITableViewController {
     }
     
     //MARK: - Data Manipulation
-   
+    
     func saveItems() {
         let encoder = PropertyListEncoder()
         
         do {
             let data = try encoder.encode(itemArray)
             try data.write(to: dataFilePath!)
-                  
         } catch {
             print(error)
         }
-        tableView.reloadData()
         
+        tableView.reloadData()
     }
     
+    func loadItems() {
+        let decoder = PropertyListDecoder()
+        
+        do {
+            if let data = try? Data(contentsOf: dataFilePath!) {
+                itemArray = try decoder.decode([Item].self, from: data)
+            }
+        } catch {
+            print(error)
+        }
+    }
     
 }
 
